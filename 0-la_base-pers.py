00556068e96212ef934fe36f706c15e5c34be393
tyc45 <tyc450@gmail.com>
2021-11-03 11:37:29 +0100

    Registering my exercices on variables.ipynb

---
+++ 0-la_base-pers.py
@@ -0,0 +1,20 @@
+print("hello world!")
+
+# Règles Pep8
+
+# Pep 8 sur les commentaires.
+# Ecrivez des phrases complètes, ponctuées et compréhensibles.
+# Le commentaire doit être cohérent avec le code.
+# Il doit suivre la même indentation que le code qu'il commente.
+# Evitez d'enfoncer des portes ouvertes : ne décrivez pas le code, expliquez plutôt à quoi il sert.
+# Il doit être en anglais.
+
+"""
+Règles Pep8
+
+Pep 8 sur les commentaires.
+Ecrivez des phrases complètes, ponctuées et compréhensibles.
+Le commentaire doit être cohérent avec le code.
+Il doit suivre la même indentation que le code qu'il commente.
+Evitez d'enfoncer des portes ouvertes : ne décrivez pas le code, expliquez plutôt à quoi il sert.
+Il doit être en anglais."""
