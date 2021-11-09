cd0e34a31e63e9d1e77c17758f2f36e27832bd48
Jordan Tyczynski <tyc450@gmail.com>
2021-11-09 11:34:37 +0100

    Added a bit of commentary

--- exercice.py
+++ exercice.py
@@ -1,8 +1,13 @@
 def my_sort(arr):
+
+    # Checking for an empty list and if every elements of list are of the same type
+
     if not arr: return []
     type_check = type(arr[0])
     for elmt in arr:
         if type_check != type(elmt): return "On mélange pas!"
+
+    # Sorting with an insertion sort algorithm
 
     for i in range(1, len(arr)):
  
