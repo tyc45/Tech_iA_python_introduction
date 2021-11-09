1584d13d2ff7137bbc95788406e4204311a03978
tyc45 <tyc450@gmail.com>
2021-11-09 09:29:03 +0100

    Created my first test!

--- exercice.py
+++ exercice.py
@@ -1,5 +1,17 @@
-def my_sort(lst):
-    lst_type = type(lst[0])
-    for elmt in lst:
-        if type(elmt) != lst_type: return "On mélange pas!"
-    sorted = []
+def my_sort(arr):
+    if not arr: return []
+    type_check = type(arr[0])
+    for elmt in arr:
+        if type_check != type(elmt): return "On mélange pas!"
+    
+    result = arr
+    for i in range(1, len(result)):
+ 
+        key = result[i]
+
+        j = i-1
+        while j >= 0 and key < result[j] :
+                result[j + 1] = result[j]
+                j -= 1
+        result[j + 1] = key
+    return result
--- test/test_file_to_test.py
+++ test/test_file_to_test.py
@@ -1,31 +1,31 @@
-import pytest
-import os
-import sys
-import inspect
+# import pytest
+# import os
+# import sys
+# import inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.insert(0, parentdir) 
 
-from file_to_test import add, add_integer, divide
+# from file_to_test import add, add_integer, divide
 
-# Il suffit de définir le test, puis écrire pytest dans le terminal suffit à le lancer
-def test_add():
-    assert add(3,4) == 7
-    assert add("a","b") == "ab"
-    #assert add(3.2,5.3) == 8.0
+# # Il suffit de définir le test, puis écrire pytest dans le terminal suffit à le lancer
+# def test_add():
+#     assert add(3,4) == 7
+#     assert add("a","b") == "ab"
+#     #assert add(3.2,5.3) == 8.0
 
-def test_add_integer():
-    assert add_integer(3,4) == 7
-    assert add_integer("a","b") == None
-    assert add_integer(3.2,5.3) == None
+# def test_add_integer():
+#     assert add_integer(3,4) == 7
+#     assert add_integer("a","b") == None
+#     assert add_integer(3.2,5.3) == None
 
-def test_divide():
-    assert divide(8,2) == 4
-    with pytest.raises(ZeroDivisionError):
-        divide(8,0)
+# def test_divide():
+#     assert divide(8,2) == 4
+#     with pytest.raises(ZeroDivisionError):
+#         divide(8,0)
 
-# on peut tester plusieurs paramètres d'un coup grâce à un décorateur.
-@pytest.mark.parametrize("numerator, numerator , result", [(2, 2, 4), ("a", "b", "ab"), (3.2, 5.3, 8.5)])
-def test_should_return_square(numerator, denominator, result):
-    assert add(numerator,numerator) == result
+# # on peut tester plusieurs paramètres d'un coup grâce à un décorateur.
+# @pytest.mark.parametrize("numerator, numerator , result", [(2, 2, 4), ("a", "b", "ab"), (3.2, 5.3, 8.5)])
+# def test_should_return_square(numerator, denominator, result):
+#     assert add(numerator,numerator) == result
