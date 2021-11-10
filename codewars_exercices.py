be6ff5b4f2ef60b3b77c2175d236062759fe5239
Jordan Tyczynski <tyc450@gmail.com>
2021-11-10 13:36:29 +0100

    stuff

--- codewars_exercices.py
+++ codewars_exercices.py
@@ -303,3 +303,12 @@ def merge(*dicts):
             else:
                 merged_dict[key] = [dict[key]]
     return merged_dict
+
+
+
+def split_in_parts(s, part_length): 
+
+    #https://www.codewars.com/kata/5650ab06d11d675371000003/train/python
+
+    lst = [s[i:i+part_length] for i in range(0, len(s), part_length)]
+    return " ".join(lst)
--- exercices_python_advance.py
+++ exercices_python_advance.py
@@ -1,31 +1,31 @@
-# # Structure générale d'une compréhension: Exo 1
+# Structure générale d'une compréhension: Exo 1
 
-# ma_liste = [3, -4, 8, 7, 20, 32, -9, 21]
-# abs_liste_impair = [i + 1 for i in ma_liste if i % 2 == 0 and i > 0]
-# print(abs_liste_impair)
-
-
-# # Structure générale d'une compréhension: Exo 2
-# lst1=[2, 4, 6, 8, 10, 12, 14]
-# big_square_list = [i ** 2 for i in lst1 if i ** 2 > 50]
-# print(big_square_list)
+ma_liste = [3, -4, 8, 7, 20, 32, -9, 21]
+abs_liste_impair = [i + 1 for i in ma_liste if i % 2 == 0 and i > 0]
+print(abs_liste_impair)
 
 
-# # Structure générale d'une compréhension: Exo 3
-# lst2 = [i for i in range(1200, 2000, 130)]
-# print(lst2)
+# Structure générale d'une compréhension: Exo 2
+lst1=[2, 4, 6, 8, 10, 12, 14]
+big_square_list = [i ** 2 for i in lst1 if i ** 2 > 50]
+print(big_square_list)
 
 
-# # Structure générale d'une compréhension: Exo 4
-# vehicules_weight = {"truck": 10000,
-#                     "plane": 50000,
-#                     "skateboard": 5,
-#                     "bike": 20,
-#                     "car": 1200,
-#                     "shoes": 0.5
-# }
-# slim_vehicles = [name.upper() for name in vehicules_weight if vehicules_weight[name] < 5000]
-# print(slim_vehicles)
+# Structure générale d'une compréhension: Exo 3
+lst2 = [i for i in range(1200, 2000, 130)]
+print(lst2)
+
+
+# Structure générale d'une compréhension: Exo 4
+vehicules_weight = {"truck": 10000,
+                    "plane": 50000,
+                    "skateboard": 5,
+                    "bike": 20,
+                    "car": 1200,
+                    "shoes": 0.5
+}
+slim_vehicles = [name.upper() for name in vehicules_weight if vehicules_weight[name] < 5000]
+print(slim_vehicles)
 
 
 # Compréhension de dictionnaire: Exo 1
